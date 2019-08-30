68958c8312acbcc9d00ae56883070bac345b2d3a
辻真緒 <>
2019-08-30 08:12:54 +0800

    js作成

--- js/message.js
+++ js/message.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 
+
+
   $(".hover").on("click", function() {
-      $(".tabpage").addClass("active");
+
+    console.log($(this).data("data-tab"));
+
+      // 選択したdata-tab に activeクラスを追加
+      $(this).addClass("active");
+
+
+      // 他のdata-tab  の activeクラスを削除
+      // $(".tabpage").removeClass("active");
+
     });
    
 });
